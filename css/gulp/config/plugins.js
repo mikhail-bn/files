981662c0ef5f9e3f4f8361ade871079a03f9413c
mikhail-bn <mihail.news@gmail.com>
2022-07-25 15:32:59 +0300

    25-07-22

--- css/gulp/config/plugins.js
+++
@@ -1,9 +0,0 @@
-import replace from "gulp-replace";
-import plumber from "gulp-plumber";
-import notify from "gulp-notify";
-
-export const plagins = {
-    replace: replace,
-    plumber: plumber,
-    notify: notify
-}
