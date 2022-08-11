e8517a3c61d7b0ec8edbe3396430ac13e0e17e7d
mikhail-bn <mihail.news@gmail.com>
2022-08-11 13:08:32 +0300

    11-08-22

---
+++ js/app36.js
@@ -0,0 +1,32 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(2 == webP.height);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = true === support ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    document.addEventListener(`click`, littleSpoiler);
+    function littleSpoiler(e) {
+        const targetElement = e.target;
+        if (targetElement.closest(`.block-next__title`)) targetElement.nextElementSibling.classList.toggle(`active`);
+    }
+    window["FLS"] = true;
+    isWebp();
+})();
