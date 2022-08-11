ba14bece71ff66ddba4034ddee4ea79f09cccdae
mikhail-bn <mihail.news@gmail.com>
2022-08-11 13:22:46 +0300

    11-08-22-1

---
+++ js/app37.js
@@ -0,0 +1,41 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(2 == webP.height);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = true === support ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    const header = document.querySelector(`.sector`);
+    const block = document.querySelector(`.some-block`);
+    const headerHeight = header.offsetHeight;
+    window.addEventListener(`scroll`, setHeaderStyle);
+    function setHeaderStyle(e) {
+        const moment = headerHeight + 10;
+        if (block.getBoundingClientRect().top <= moment) {
+            header.style.top = `${block.offsetTop - moment}px`;
+            header.classList.add(`active`);
+        } else {
+            header.classList.remove(`active`);
+            header.style.top = ``;
+        }
+    }
+    window["FLS"] = true;
+    isWebp();
+})();
