367783fa0f64bd556ef3231154d99ff9d713dac8
mikhail-bn <mihail.news@gmail.com>
2022-08-31 17:20:29 +0300

    Счётчик символов

---
+++ js/app44.js
@@ -0,0 +1,39 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(2 == webP.height);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = true === support ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    const txtItem = document.querySelector(`.form__textarea`);
+    const txtItemLimit = txtItem.getAttribute(`maxlength`);
+    const txtCounter = document.querySelector(`.textarea__counter span`);
+    txtCounter.innerHTML = txtItemLimit;
+    txtItem.addEventListener(`keyup`, txtSetCounter);
+    txtItem.addEventListener(`keydown`, (function(event) {
+        if (event.repeat) txtSetCounter();
+    }));
+    function txtSetCounter() {
+        const txtCounterResult = txtItemLimit - txtItem.value.length;
+        txtCounter.innerHTML = txtCounterResult;
+    }
+    window["FLS"] = true;
+    isWebp();
+})();
